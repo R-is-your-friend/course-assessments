dc737f66363582906d160f560c810097c4b87692
Allison Theobold <atheobol@calpoly.edu>
2026-06-21 18:36:25 -0700

    using versions for PA 3

---
+++ practice-activities/student/pa3-control-flow.R
@@ -0,0 +1,51 @@
+## Part One: Getting the Drink Orders
+
+# Question 1: Let's get the drink order data into `R`. Since we haven't covered
+# reading in data yet, we'll make it easy! Follow these steps.
+# 1.  In RStudio, go to the *Files* pane and navigate to the saved data.
+# 2.  Click on the file name and select *Import Dataset...*.
+# 3.  Keep all of the settings and click *Import*.
+
+# You should now see the `coffee_orders` dataset in your Environment tab (upper
+# right corner)!
+
+## Part Two: Getting Information About the Drink Orders
+
+## Question 2: How many drinks are in the order? Save this number in an object
+# named num_orders and print out the value.
+
+
+# Question 3: Think back to last week. What operator allows you to grab a single
+# column from a dataframe? Using that operator, write a line of code that
+# produces a vector of the drink Base for all drinks.
+
+
+# Question 4: Now write a line of code that produces the drink Base for the
+# fourth drink order.
+
+
+## Part Three: Finding Specific Drink Orders
+
+# Question 5: Use a while() loop to find the first person to order a drink with
+# a chai Base. Report this person's full drink order.
+
+
+# Question 6: Use a for() loop to count the total number of espresso drinks.
+
+
+## Part Four: Making Drink Modifications
+
+# Question 7: Oh no! Tidy Roasters has run out of soy milk. The baRistas decide
+# to use oat milk instead. In the coffee_orders dataframe, change all the soy
+# milk requests to oat milk. Use the if_else() function from `dplyr` to do this.
+
+
+# Question 8: The baRistas need to know how much ice to have on hand. For a
+# medium iced drink, they need 4oz of ice. For a large iced drink, they need 6oz
+# of ice. Use a for() loop and conditionals to calculate the total ice amount
+# the baRistas need.
+
+
+## Canvas Submission
+
+# You will submit your answers to Questions 2, 5, 6, and 8 on Canvas.
