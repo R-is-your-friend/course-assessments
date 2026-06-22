935cf24cd616d681b7a951a962fb89479b1390ef
manncz <czmann@calpoly.edu>
2026-06-22 09:32:29 -0700

    move a couple things around and clean render with versioning

---
+++ labs/student/lab-1.R
@@ -0,0 +1,29 @@
+## Creating Objects
+
+# Question 1: Create three objects: x, y, z
+# Each object should have a different numerical value
+
+
+# Question 2: Create an object a, that contains the word "hello"
+
+
+## Using Simple Functions
+
+# Question 3: Use the mean() function to find the average of x, y, and z
+
+
+# Question 4: Use the seq() function to make a sequence of numbers from 1 to 10
+
+
+## Combining Values
+
+# Question 5: Use the c() function to combine x, y, and z into a **single** vector
+# Give the object an intuitive name!
+
+
+# Question 6: Use the `c()` function again to add `a` into the vector and display the result.
+# You do not need to create an object here!
+
+
+# Question 7: What happened when you combined a with the vecotr of values?
+# Why do you think that happened?**
